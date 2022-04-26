f28f5b0e9234fdd39c56840fe8128158ea0bcd3d
Wendeljrk <wj-r@hotmail.com>
2022-04-25 22:58:50 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+function handleClick(){
+    var wpp = document.querySelector("icon whatsapp")
+    var maisIm = document.querySelector("maisIm")
+    var callMe = document.querySelector("callMe")
+    debugger
+    if ( wpp.style.display === "none" ){
+        wpp.style.display = "block"
+    }
+    console.log(wpp)
+}
